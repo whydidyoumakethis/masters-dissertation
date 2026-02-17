e785ceeadddd672bc0dbd148c82c2870c760c49f
samw-leeds <sc22sw@leeds.ac.uk>
2026-02-17 13:56:34 +0000

    added glm

--- games/demo/main.cpp
+++ games/demo/main.cpp
@@ -1,6 +1,11 @@
 #include <temp.h>
+#include <glm/vec3.hpp>
 
 int main(int argc, char**argv) {
     temp::print("demo");
+    glm::vec3 vector(0.1f, 0.2f, 3.f);
+
+    std::cout << vector.r << " " << vector.g << " " << vector.b << std::endl;
+
     return 0;
 }
