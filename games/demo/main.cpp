adb9132715be429db1c9e4296651ef89e3fb9e33
samw-leeds <sc22sw@leeds.ac.uk>
2026-02-17 15:00:32 +0000

    added glfw

--- games/demo/main.cpp
+++ games/demo/main.cpp
@@ -3,13 +3,21 @@
 
 #include <volk.h>
 
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
 int main(int argc, char**argv) {
     temp::print("demo");
     glm::vec3 vector(0.1f, 0.2f, 3.f);
 
     std::cout << vector.r << " " << vector.g << " " << vector.b << std::endl;
 
-    VkResult volkInitilialize();
+    // VkResult volkInitilialize();
+
+    // glfwSetCharCallback();
+
+    if (!glfwInit())
+        return -1;
 
     return 0;
 }
