35a380453f720b4bc19a964ca7f2e8fde5bffef1
Sept. <305795689@qq.com>
2026-02-21 06:29:59 +0800

    fix merge bug

--- games/demo/main.cpp
+++ games/demo/main.cpp
@@ -38,7 +38,13 @@ void update(entt::registry& registry) {
     //}
 }
 
-#include <renderer/render.h>
+int main(int argc, char** argv) {
+    temp::print("demo");
+    glm::vec3 vector(0.1f, 0.2f, 3.f);
+
+    std::cout << vector.r << " " << vector.g << " " << vector.b << std::endl;
+
+    // VkResult volkInitilialize();
 
     // glfwSetCharCallback();
 
