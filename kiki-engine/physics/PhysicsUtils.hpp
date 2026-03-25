42266d7559f6656212eea4e95c4fde5414f367d7
cubertony <1762170534@qq.com>
2026-03-25 13:06:30 +0000

    Now the physics is attached to the loaded objects, assuming all of them are static

--- kiki-engine/physics/PhysicsUtils.hpp
+++ kiki-engine/physics/PhysicsUtils.hpp
@@ -4,6 +4,10 @@
 #include <glm/gtc/quaternion.hpp>
 #include <Jolt/Jolt.h>
 
+#include <vector>
+#include <Jolt/Physics/Collision/Shape/MeshShape.h>
+#include <Jolt/Physics/Collision/Shape/ConvexHullShape.h>
+
 namespace Kiki {
 
     // --- GLM to Jolt ---
@@ -41,5 +45,39 @@ namespace Kiki {
 
     inline glm::quat ToGLM(const JPH::Quat& q) {
         return glm::quat(q.GetW(), q.GetX(), q.GetY(), q.GetZ());
+    }
+
+    //for static colliders
+    static JPH::Ref<JPH::Shape> CreateTriangleMesh(const std::vector<glm::vec3>& vertices, const std::vector<uint32_t>& indices) {
+        JPH::VertexList joltVertices;
+        joltVertices.reserve(vertices.size());
+        for (const auto& v : vertices) {
+            joltVertices.push_back(JPH::Float3(v.x, v.y, v.z));
+        }
+
+        JPH::IndexedTriangleList joltIndices;
+        joltIndices.reserve(indices.size() / 3);
+        for (size_t i = 0; i < indices.size(); i += 3) {
+			//keep in range check
+            joltIndices.push_back(JPH::IndexedTriangle(indices[i], indices[i + 1], indices[i + 2]));
+        }
+
+        JPH::MeshShapeSettings settings(joltVertices, joltIndices);
+        JPH::Shape::ShapeResult result = settings.Create();
+
+        return result.IsValid() ? result.Get() : nullptr;
+    }
+
+	// ConvexHull for dynamic colliders, but we still not using it in the current version.
+    static JPH::Ref<JPH::Shape> CreateConvexHull(const std::vector<glm::vec3>& vertices) {
+        JPH::ConvexHullShapeSettings settings;
+        for (const auto& v : vertices) {
+            settings.mPoints.push_back(ToJPH(v));
+        }
+
+        settings.mMaxConvexRadius = 0.05f;
+
+        JPH::Shape::ShapeResult result = settings.Create();
+        return result.IsValid() ? result.Get() : nullptr;
     }
 }
