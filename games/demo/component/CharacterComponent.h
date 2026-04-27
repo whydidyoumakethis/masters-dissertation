eba0867f751253cb294300ecc866022d0d2f6ce6
Sept. <305795689@qq.com>
2026-04-28 07:58:33 +0800

    change jump setting

--- games/demo/component/CharacterComponent.h
+++ games/demo/component/CharacterComponent.h
@@ -22,7 +22,7 @@ inline std::string to_string(CharacterState state) {
 struct CharacterComponent {
     float walkSpeed = 5.0f;
     float runSpeed = 18.0f;
-    float jumpForce = 50.0f;
+    float jumpForce = 15.0f;
 	float rotateSpeed = 10.0f; // character rotation speed (for interpolating facing direction)
 
     CharacterState state = CharacterState::Idle;
